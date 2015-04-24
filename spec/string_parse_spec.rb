4340d09eb4fa0ba2103dfc73abad74c8c0168a3c
Cody Bouscaren <codybousc@gmail.com>
2015-04-24 10:48:39 -0700

    It passed the second spec

--- spec/string_parse_spec.rb
+++ spec/string_parse_spec.rb
@@ -5,4 +5,7 @@ describe('String#string_sparse') do
   it ('returns the quantity of times that a given word occurs in a user provided string') do
     expect(("I'm happy").string_parse("happy")).to(eq(1))
   end
+  it ('returns the quantity of times that a given word occurs in a user provided string') do
+    expect(("This day is a great day").string_parse("day")).to(eq(2))
+  end
 end
