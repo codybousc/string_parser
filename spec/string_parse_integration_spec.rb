03a928c91a4b1c01aa5a8dd754eba55a525d9ec8
Cody Bouscaren <codybousc@gmail.com>
2015-04-28 21:27:17 -0700

    Now that I've got all specs to pass, the web app no longer works; it doesn't return the number of times the word appeared.

--- spec/string_parse_integration_spec.rb
+++ spec/string_parse_integration_spec.rb
@@ -6,8 +6,8 @@ set(:show_exceptions, false)
 describe("returns the number of times a word appears in a user provided string", {:type => :feature}) do
   it("processes the user entry and returns correct number of instances of given word") do
     visit('/')
-    fill_in("sentence", :with => 'happy happy chap')
-    fill_in("word", :with => 'happy')
+    fill_in("sentence", :with => "happy happy chap")
+    fill_in("word", :with => "happy")
     click_button('Submit String')
     expect(page).to have_content("2")
   end
